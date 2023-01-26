e97660187fcb10fb773a4c2a38eb14d9e381272f
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-01-26 23:08:13 +0100

    change title

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Spanduck is ...",
   viewport: "width=device-width,initial-scale=1",
 });
 
