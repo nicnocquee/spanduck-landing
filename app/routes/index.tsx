b0cde4dca18e7d3d89a72d06f54c860476841d04
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-03-16 22:22:48 +0100

    add link to the app

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -16,7 +16,7 @@ export default function Index() {
         </h1>
       </div>
       <a
-        href="#_"
+        href="https://app.spanduck.hyperjump.tech/"
         className="relative self-start px-6 py-3 font-bold text-black group"
       >
         <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
